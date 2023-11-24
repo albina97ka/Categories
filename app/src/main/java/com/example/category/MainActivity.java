51dc2fc65ee20034749d7070ee9559098507c9eb
albina97ka <albina97ka@gmail.com>
2023-11-24 21:37:17 +0300

    init

--- app/src/main/java/com/example/category/MainActivity.java
+++ app/src/main/java/com/example/category/MainActivity.java
@@ -9,14 +9,20 @@ import android.widget.ListView;
 
 import java.io.File;
 import java.util.Arrays;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        refresh();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container_view, ContentFragment.class, null)
+                    .commit();
+        }
     }
 
     @Override
